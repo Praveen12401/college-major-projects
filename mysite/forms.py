13aac0e36ca5245a6199f2b17b36e5ce9f5c6098
praveen <praveenyadav16178@gmail.com>
2024-01-22 10:16:07 +0530

    ok

---
+++ mysite/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class Userform(forms.Form):
+    Name=forms.CharField()
+    Branch=forms.CharField()
+    Smester=forms.IntegerField()
+    email = forms.EmailField()
+    age=forms.DateField()
+''' create a login form here class'''
+class LoginForm(forms.Form):
+    username=forms.CharField(max_length=20)
+    password=forms.CharField(widget=forms.PasswordInput)
+    
